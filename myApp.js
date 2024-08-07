223809d4d57a9560d3db21e423ab981e69f6c29b
ahmed abdullah <aa2601959@gmail.com>
2024-08-07 15:43:52 +0300

    make person schema

--- myApp.js
+++ myApp.js
@@ -1,7 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  });
 
-let Person;
+const personSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
